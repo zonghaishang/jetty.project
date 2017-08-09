fe50437a034016c32975e28dcc717ddf30c726d7
Simone Bordet <simone.bordet@gmail.com>
2017-08-09 14:06:27 +0200

    Fixed log that was throwing NPE at DEBUG level in certain cases.

--- jetty-server/src/main/java/org/eclipse/jetty/server/HttpChannel.java
+++ jetty-server/src/main/java/org/eclipse/jetty/server/HttpChannel.java
@@ -100,7 +100,11 @@ public class HttpChannel implements Runnable, HttpOutput.Interceptor
         _requestLog = connector == null ? null : connector.getServer().getRequestLog();
 
         if (LOG.isDebugEnabled())
-            LOG.debug("new {} -> {},{},{}",this,_endPoint,_endPoint.getConnection(),_state);
+            LOG.debug("new {} -> {},{},{}",
+                    this,
+                    _endPoint,
+                    _endPoint==null?null:_endPoint.getConnection(),
+                    _state);
     }
 
     protected HttpInput newHttpInput(HttpChannelState state)
